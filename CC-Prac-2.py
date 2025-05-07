8ac801ad3b7ebbb8e238ea220e5ce9bc575c8fb6
Rohan Singh <rohans212004@gmail.com>
2025-05-07 12:15:41 +0530

    python code changes

--- CC-Prac-2.py
+++ CC-Prac-2.py
@@ -1,2 +1,9 @@
-print('hello')
-print('te')
+print("Hello, World!")
+
+n = int(input("Enter a number: "))
+print("")
+
+for i in range(n):
+    for j in range(n):
+        print("*", end="")  
+    print() 
